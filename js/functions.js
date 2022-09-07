a652c979ecd7acb358ee5a4b4b6ad2dfd7600921
FrenzH <franciattom99@gmail.com>
2022-09-07 16:35:33 +0100

    add-function.js

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+
+
+function moveCarouselForward(){
+    clearInterval(idInterval)
+    activeIndex = activeIndex < images.length -1 ? activeIndex +1 : 0 ;
+    buildCarousel(images, activeIndex);
+    idInterval = setInterval(moveCarouselForward, CHANGE_IMAGE_DELAY * 1000);
+}
+
+function moveCarouselPrevious(){
+     clearInterval(idInterval);
+    activeIndex = activeIndex > 0 ? activeIndex -1 : images.length -1 ;
+    buildCarousel(images, activeIndex);
+    idInterval = setInterval(moveCarouselForward, CHANGE_IMAGE_DELAY * 1000);
+}
+
+
+function buildCarousel(images, activeIndex){
+    const carouselImages = document.querySelector('.carousel-images');
+    const carouselThumbs = document.querySelector('.carousel-thumbs');
+    let content = '';
+    for(let i = 0; i < images.length; i++){
+        const url = images[i].url;
+        const title = images[i].title;
+        const imageClass = i === activeIndex ? 'carousel-img active' : 'carousel-img'
+        content += `<img class="${imageClass}" src="${url}" alt="${title}" />`;
+    }
+    carouselImages.innerHTML = content;
+    carouselThumbs.innerHTML = content;
+}
+
--- js/script.js
+++ js/script.js
@@ -31,20 +31,20 @@ const images = [
 ];
 
 
-const CHANGE_IMAGE_DELAY = 5;
-
-/*const images = createImageArray(NUM_IMAGES);*/
-console.log(images[0].url);
-
-
-
+const CHANGE_IMAGE_DELAY = 8;
 let activeIndex = 0;
+
+
+
 buildCarousel(images, activeIndex);
 
 let idInterval = setInterval(moveCarouselForward, CHANGE_IMAGE_DELAY * 1000);
 
 const leftArrowButton = document.getElementById('left-arrow');
 const rightArrowButton = document.getElementById('right-arrow');
+
+
+
 
 leftArrowButton.addEventListener('click', moveCarouselPrevious);
 
@@ -53,48 +53,3 @@ rightArrowButton.addEventListener('click', moveCarouselForward);
 
 
 
-
-
-function moveCarouselForward(){
-    clearInterval(idInterval)
-    // se l'indice si trova in fondo allora lo riposizione all'inizio dell'array
-    activeIndex = activeIndex < images.length -1 ? activeIndex +1 : 0 ;
-    buildCarousel(images, activeIndex);
-    idInterval = setInterval(moveCarouselForward, CHANGE_IMAGE_DELAY * 1000);
-}
-
-function moveCarouselPrevious(){
-     clearInterval(idInterval)
-    // se l'indice è in prima posizione si valorizza all'ultima posizione dell'array
-    activeIndex = activeIndex > 0 ? activeIndex -1 : images.length -1 ;
-    buildCarousel(images, activeIndex);
-    idInterval = setInterval(moveCarouselForward, CHANGE_IMAGE_DELAY * 1000);
-}
-
-
-function buildCarousel(images, activeIndex){
-    const carouselImages = document.querySelector('.carousel-images');
-    const carouselThumbs = document.querySelector('.carousel-thumbs');
-    let content = '';
-    for(let i = 0; i < images.length; i++){
-        const url = images[i].url;
-        const title = images[i].title;
-        const imageClass = i === activeIndex ? 'carousel-img active' : 'carousel-img'
-        content += `<img class="${imageClass}" src="${url}" alt="${title}" />`;
-    }
-    // console.log({content});
-    carouselImages.innerHTML = content;
-    carouselThumbs.innerHTML = content;
-}
-
-/*
-function createImageArray(numImages){
-    const images = [];
-    for(let i = 1; i <= numImages; i++){
-        const fileName = i < 10 ? '0' + i : i;
-        const url = 'img/' + fileName + '.jpg';
-        images.push(url); 
-    }
-
-    return images;
-}*/
